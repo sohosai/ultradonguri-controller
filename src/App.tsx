725c30a99df268df9d44f6bbee4dac5dce8e4e9f
pocopota <pocopota.program@gmail.com>
2025-10-04 21:47:16 +0900

    モックデータのフェッチとサンプル表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,62 @@
-import { useState } from "react";
-
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  type Music = {
+    id: string;
+    title: string;
+    artist: string;
+    should_be_muted: boolean;
+    intro: string;
+  };
+
+  type Performance = {
+    id: string;
+    title: string;
+    performer: string;
+    description: string;
+    starts_at: string;
+    ends_at: string;
+    musics: Music[];
+  };
+
+  const [data, setData] = useState<Performance[] | null>(null);
+
+  useEffect(() => {
+    // データフェッチはバックエンドが出来たら変更する
+    const url = `${import.meta.env.BASE_URL}mock.json`;
+    (async () => {
+      try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error(`Failed to fetch: ${res.status}`);
+        const json = (await res.json()) as Performance[];
+        setData(json);
+      }catch(e){
+        console.error(e);
+        setData([]);
+      }
+    })();
+  }, []);
+  if (!data) return <div>データが見つかりません。</div>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1>Ultradonguri</h1>
+      <h2>パフォーマンス一覧</h2>
+      <ul>
+        {data.map((p) => (
+          <li key={p.id}>
+            <div>{p.title}</div>
+            <ul>
+              {p.musics.map((m) => (
+                <li key={m.id}>
+                  {m.title}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
