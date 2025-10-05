3233678fb311587c400776664b4f1b3a4fb9f059
pocopota <pocopota.program@gmail.com>
2025-10-05 23:16:12 +0900

    全体フォントの指定

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 import "./styles/colors.css";
+import "./styles/typography.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
