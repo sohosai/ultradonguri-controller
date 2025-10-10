bc10d1d9dc68227ddbafb432f6fb8f02d822c9b2
pocopota <pocopota.program@gmail.com>
2025-10-10 18:53:59 +0900

    選択済みのPerformanceItemのサイズの変更

--- src/components/Performances/index.tsx
+++ src/components/Performances/index.tsx
@@ -6,15 +6,16 @@ import type { Performance } from "../../types/performances";
 
 type Props = {
   items: Performance[];
+  selectedPerformance?: Performance | null;
   onSelect?: (performance: Performance) => void;
 };
 
-export default function Performances({ items, onSelect }: Props) {
+export default function Performances({ items, selectedPerformance, onSelect }: Props) {
   return (
     <ul className={styles.performances}>
       {items.map((p) => (
         <li key={p.id}>
-          <PerformanceItem performance={p} onSelect={onSelect} />
+          <PerformanceItem performance={p} isSelected={selectedPerformance?.id === p.id} onSelect={onSelect} />
         </li>
       ))}
     </ul>
