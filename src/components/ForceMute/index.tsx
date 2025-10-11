b025d628ab3ad5d0317b08cd267223b690ef32c6
yazo <rixiaduotiben@gmail.com>
2025-10-11 16:02:55 +0900

    ボタンのスタイル

--- src/components/ForceMute/index.tsx
+++ src/components/ForceMute/index.tsx
@@ -1,5 +1,14 @@
 import styles from "./index.module.css";
 
 export default function ForceMute() {
-  return <div className={styles.forceMute}>強制ミュート</div>;
+  return (
+    <div className={styles.tigerStripe}>
+        <div className={styles.forceMute}>
+              強制
+              <br />
+             ミュート
+           <div className={styles.glass}></div>
+      </div>
+    </div>
+  );
 }
