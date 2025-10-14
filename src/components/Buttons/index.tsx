2ff1a060d2ece1a650c3f1d86bd8dd30cd9203cc
sora <sorahanamoto0319@gmail.com>
2025-10-14 17:56:33 +0900

    著作権表示トグルコンポーネント

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,16 +1,20 @@
 import ForceMute from "../ForceMute";
+import CopyrightToggle from "../CopyrightToggle";
 import NextTrackButton from "../NextTrackButton";
 
 import styles from "./index.module.css";
 
 type Props = {
+  isCopyrightVisible: boolean;
+  onCopyrightVisibleChange?: (v: boolean) => void;
   onNext?: () => void;
 };
 
-export default function Buttons({ onNext }: Props) {
+export default function Buttons({ isCopyrightVisible, onCopyrightVisibleChange, onNext }: Props) {
   return (
     <div className={styles.buttons}>
       <ForceMute />
+      <CopyrightToggle isCopyrightVisible={isCopyrightVisible} onChange={onCopyrightVisibleChange}/>
       <NextTrackButton onNext={onNext} />
     </div>
   );
---
+++ src/components/CopyrightToggle/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./index.module.css";
+
+type Props = {
+  isCopyrightVisible: boolean;
+  onChange?: (checked: boolean) => void;
+};
+
+export default function CopyrightToggle({ isCopyrightVisible, onChange }: Props) {
+  return (
+    <div className={styles.copyright}>
+      <div className={styles.copyrightTitle}>著作権表示</div>
+      <div className={styles.ToggleTextOff}>OFF</div>
+      <div className={styles.toggleButton}>
+        <label className={styles.switch}>
+          <input
+            type="checkbox"
+            checked={isCopyrightVisible}
+            onChange={(e) => onChange?.(e.target.checked)}
+          />
+          <span className={styles.slider} aria-hidden="true" />
+        </label>
+      </div>
+      <div className={styles.ToggleTextOn}>ON</div>
+    </div>
+  );
+}
