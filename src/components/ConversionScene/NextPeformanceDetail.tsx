35e4b2ebc64542b69b925ed878ca34b984d0a908
pocopota <pocopota.program@gmail.com>
2025-10-15 21:11:49 +0900

    デザインを整える

--- src/components/ConversionScene/NextPeformanceDetail.tsx
+++ src/components/ConversionScene/NextPeformanceDetail.tsx
@@ -1,5 +1,7 @@
 import type { NextPerformance } from "../../types/performances";
 
+import styles from "./NextPerformanceDetail.module.css";
+
 type ConversionSceneProps = {
   nextPerformances: NextPerformance[];
 };
@@ -8,10 +10,18 @@ export default function NextPerformanceDetail({ nextPerformances }: ConversionSc
   const performance = nextPerformances[0];
 
   return (
-    <div>
-      <div>{performance.title}</div>
-      <div>{performance.performer}</div>
-      <div>{performance.starts_at}</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.left}>
+          <div className={styles.starts_at}>{performance.starts_at}</div>
+          <div className={styles.nextLabel}>NEXT</div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.title}>{performance.title}</div>
+          <div className={styles.performer}>{performance.performer}</div>
+        </div>
+      </div>
+      <div className={styles.description}>{performance.description}</div>
     </div>
   );
 }
--- src/components/ConversionScene/NextPerformances.tsx
+++ src/components/ConversionScene/NextPerformances.tsx
@@ -1,21 +1,26 @@
 import type { NextPerformance } from "../../types/performances";
 
+import styles from "./NextPerformances.module.css";
+
 type ConversionSceneProps = {
   nextPerformances: NextPerformance[];
 };
 
 export default function NextPerformances({ nextPerformances }: ConversionSceneProps) {
   return (
-    <div>
-      <div>
-        {nextPerformances.slice(0, 3).map((performance, index) => (
-          <div key={index}>
-            <div>{performance.title}</div>
-            <div>{performance.performer}</div>
-            <div>{performance.starts_at}</div>
+    <div className={styles.container}>
+      {nextPerformances.slice(0, 3).map((performance, index) => (
+        <div key={index} className={styles.item}>
+          <div className={styles.left}>
+            <div className={styles.starts_at}>{performance.starts_at}</div>
+            {index === 0 && <div className={styles.nextLabel}>NEXT</div>}
           </div>
-        ))}
-      </div>
+          <div className={styles.right}>
+            <div className={styles.title}>{performance.title}</div>
+            {index === 0 && <div className={styles.performer}>{performance.performer}</div>}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
