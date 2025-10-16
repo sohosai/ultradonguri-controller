f4a2559faaa239c2ce6cd5e650bac2be98d94adc
yazo <rixiaduotiben@gmail.com>
2025-10-16 22:39:37 +0900

    修正

--- src/components/Header/clock.tsx
+++ src/components/Header/clock.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import styles from "./index.module.css";
 
 const formatTime = (date: Date) => {
-  const month = String(date.getMonth());
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
