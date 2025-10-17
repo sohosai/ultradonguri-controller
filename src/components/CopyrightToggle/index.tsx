5cacfd95af35a0411969d147c90417618868f3d2
sora <sorahanamoto0319@gmail.com>
2025-10-17 18:18:52 +0900

    ToggleコンポーネントをGridからFlexboxに。ホバー時にポインター表示に

--- src/components/CopyrightToggle/index.tsx
+++ src/components/CopyrightToggle/index.tsx
@@ -11,11 +11,7 @@ export default function CopyrightToggle({ isCopyrightVisible, onChange }: Props)
   return (
     <div className={styles.copyright}>
       <div className={styles.copyrightTitle}>著作権表示</div>
-      <div className={styles.ToggleTextOff}>OFF</div>
-      <div className={styles.toggleButton}>
-        <Toggle checked={isCopyrightVisible} onChange={onChange} />
-      </div>
-      <div className={styles.ToggleTextOn}>ON</div>
+      <Toggle checked={isCopyrightVisible} onChange={onChange} />
     </div>
   );
 }
--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -2,14 +2,22 @@ import styles from "./index.module.css";
 
 type Props = {
   checked: boolean;
-  onChange: (next: boolean) => void;
+  onChange: (checked: boolean) => void;
 };
 
 export default function Toggle({ checked, onChange }: Props) {
   return (
-    <label className={styles.switch}>
-      <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
-      <span className={styles.slider} aria-hidden="true" />
-    </label>
+    <div className={styles.toggleRow}>
+      <div className={styles.toggleText}>OFF</div>
+
+      <div className={styles.toggleButton}>
+        <label className={styles.switch}>
+          <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
+          <span className={styles.slider} aria-hidden="true" />
+        </label>
+      </div>
+
+      <div className={styles.toggleText}>ON</div>
+    </div>
   );
 }
