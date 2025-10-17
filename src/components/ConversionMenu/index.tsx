79ced8e536f922de7abd45b868e5d5d2b818f9e5
yazo <rixiaduotiben@gmail.com>
2025-10-17 20:42:50 +0900

    転換パートのトグルボタン表示

--- src/components/ConversionMenu/index.tsx
+++ src/components/ConversionMenu/index.tsx
@@ -1,3 +1,5 @@
+import Toggle from "../Toggle";
+
 import styles from "./index.module.css";
 
 export default function ConversionMenu() {
@@ -5,7 +7,10 @@ export default function ConversionMenu() {
     <div className={styles.conversionMenu}>
       <h2 className={styles.title}>Conversion</h2>
       <div className={styles.info}>
-        <p>CM ON/OFF</p>
+        <p>CM</p>
+        <p>
+          <Toggle />
+        </p>
       </div>
     </div>
   );
