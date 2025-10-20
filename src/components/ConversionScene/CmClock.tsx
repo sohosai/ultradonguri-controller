9231cd34d434cb62091227b2cf6c7880e136d97e
yazo <rixiaduotiben@gmail.com>
2025-10-20 18:50:46 +0900

    枠

---
+++ src/components/ConversionScene/CmClock.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+const formatTime = (date: Date): { hours: string; minutes: string } => {
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return { hours, minutes };
+};
+
+const CmClock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [isColonVisible, setIsColonVisible] = useState(true);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+  useEffect(() => {
+    const blinkId = setInterval(() => {
+      setIsColonVisible((prev) => !prev);
+    }, 500);
+
+    return () => clearInterval(blinkId);
+  }, []);
+
+  const { hours, minutes } = formatTime(currentTime);
+  const colon = isColonVisible ? ":" : " ";
+
+  return (
+    <>
+      <span>{hours}</span>
+      <span style={{ width: "15px", display: "Block" }}>{colon}</span>
+      <span>{minutes}</span>
+    </>
+  );
+};
+
+export default CmClock;
--- src/components/ConversionScene/CmMode.tsx
+++ src/components/ConversionScene/CmMode.tsx
@@ -1,3 +1,4 @@
+import CmClock from "./CmClock";
 import styles from "./CmMode.module.css";
 
 import type { NextPerformance } from "../../types/performances";
@@ -9,13 +10,17 @@ type CmModeProps = {
 export default function CmMode({ nextPerformances }: CmModeProps) {
   return (
     <div className={styles.container}>
-      <div className={styles.text}>CM</div>
-      <div className={styles.debugInfo}>
-        {nextPerformances.map((performance, index) => (
-          <div key={index}>
-            {performance.starts_at} - {performance.title} ({performance.performer})
+      <div className={styles.top}>
+        <div className={styles.topGuides}>
+          <div className={styles.clock}>
+            <CmClock />
           </div>
-        ))}
+          <div className={styles.nextGuide}>次の団体の紹介部分</div>
+        </div>
+        <div className={styles.broadcast}>配信画面</div>
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.laterGuide}>次以降の団体の紹介</div>
       </div>
     </div>
   );
