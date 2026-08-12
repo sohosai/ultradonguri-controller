e07994babf78659274106fe8a9e21f6776992eec
sugi404 <199529692+sugi404@users.noreply.github.com>
2026-08-12 18:09:04 +0900

    団体追加メニューモーダルUI改善

--- src/components/DetailMenu/SampleModal.tsx
+++ src/components/DetailMenu/SampleModal.tsx
@@ -1,17 +1,31 @@
 import styles from "./SampleModal.module.css";
 
-type SampleModalProps ={
-    isOpen: boolean;
+type SampleModalProps = {
+  isOpen: boolean;
 };
 
-export default function SampleModal({isOpen}: SampleModalProps) {
-    if(!isOpen) return null;
+export default function SampleModal({ isOpen }: SampleModalProps) {
+  if (!isOpen) return null;
 
-    return (
-        <div className={styles.modalOverlay}>
-            <div className={styles.modalContent}>
-                <h1>Sample Modal</h1>
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <div className={styles.modalHeader}>団体追加メニュー</div>
+        <div className={styles.modalBody}>
+          <div className={styles.items}>
+            <div className={styles.details}>
+              <div className={styles.detailItem}>
+                <label>団体名</label>
+                <input type="text" className={styles.input} />
+              </div>
             </div>
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.cancel}>キャンセル</button>
+            <button className={styles.save}>保存</button>
+          </div>
         </div>
-    );    
+      </div>
+    </div>
+  );
 }
